1bc660088e80c606da089d34854f3ae4961ee64e
BigMaestrov <nikita.egorov40k@gmail.com>
2023-03-25 13:55:19 +0400

    Вынес функциональность по расчету стоимости в метод calculateTotalCost.

--- src/main/java/org/example/Abonent.java
+++ src/main/java/org/example/Abonent.java
@@ -26,6 +26,7 @@ public class Abonent {
     @Override
     public String toString() {
         String stringCalls = "";
+        calculateTotalCost();
         double cost = getTotalCost();
         for (CDR buff : calls) {
             stringCalls += buff.toString();
@@ -68,6 +69,10 @@ public class Abonent {
 
     //Подсчет итоговой стоимости
     public double getTotalCost() {
+        return totalCost;
+    }
+
+    public void calculateTotalCost(){
         for (int i = 0; i < calls.size(); i++) {
 
             switch (calls.get(i).getTariffType().trim()) {
@@ -104,24 +109,23 @@ public class Abonent {
 
                 //Тариф безлимит
                 case ("06"):
-                        //Если в период от начала до завершения разговора НЕ превышен лимит в 300 минут
-                        if (getTotalDuration().getTime() < 300 * 60 * 1000 && (getTotalDuration().getTime()+calls.get(i).getDuration().getTime()) < 100 * 60 * 1000) {
-                            calls.get(i).setCost(0.0);
-                            setTotalCost(100);
-                            //Если на момент начала разговора ПРЕВЫШЕН лимит в 300 минут
-                        } else if(getTotalDuration().getTime() > 100 * 60 * 1000) {
-                            calls.get(i).setCost((calls.get(i).getDuration().getTime() + 14400000) / 60_000 * 1.0);
-                            //Если лимит будет превышен в момент разговора
-                        } else {
-                            double start = 100 * 60 * 1000 - calls.get(i).getStartOfCall().getTime();
-                            double end = calls.get(i).getEndOfCall().getTime() - 100 * 60 * 1000;
-                            calls.get(i).setCost((end + 14400000) / 60_000 * 1);
-                        }
+                    //Если в период от начала до завершения разговора НЕ превышен лимит в 300 минут
+                    if (getTotalDuration().getTime() < 300 * 60 * 1000 && (getTotalDuration().getTime()+calls.get(i).getDuration().getTime()) < 100 * 60 * 1000) {
+                        calls.get(i).setCost(0.0);
+                        setTotalCost(100);
+                        //Если на момент начала разговора ПРЕВЫШЕН лимит в 300 минут
+                    } else if(getTotalDuration().getTime() > 100 * 60 * 1000) {
+                        calls.get(i).setCost((calls.get(i).getDuration().getTime() + 14400000) / 60_000 * 1.0);
+                        //Если лимит будет превышен в момент разговора
+                    } else {
+                        double start = 100 * 60 * 1000 - calls.get(i).getStartOfCall().getTime();
+                        double end = calls.get(i).getEndOfCall().getTime() - 100 * 60 * 1000;
+                        calls.get(i).setCost((end + 14400000) / 60_000 * 1);
+                    }
                     totalCost += calls.get(i).getCost();
                     break;
             }
         }
-        return totalCost;
     }
 
     //Подсчет промежуточной стоимости
